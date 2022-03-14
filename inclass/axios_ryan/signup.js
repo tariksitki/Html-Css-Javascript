756faae96f18d16281abad031ee85f18b61d6362
tarik <63872091+tariksitki@users.noreply.github.com>
2022-03-14 12:26:51 +0100

    axios lesson overview completed

--- inclass/axios_ryan/signup.js
+++ inclass/axios_ryan/signup.js
@@ -40,6 +40,106 @@
 
 // bu gönderdiklerimizi, postman da row ile degilde x www yöntemi ile de gönderdik. bu yöntem form yöntemidir
 
-// 
+
+
+
+/// Note: bu sayfadaki islemlerde biz yeni bir kullanici register edemiyoruz ama register api kullaniyoruz ve eve holt isimli kisiyi register ediyoruz.
+// kullandigimiz api sitesinde register api bize token ve email verir
+// login api ise sadece token verir
+
+
+
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submitBtn = document.getElementById("submit");
+
+
+// bu tanimlamalari console.log ile yazmayagerek yok kontrol icin. bunlari kopyalayip direkt console a yapistirabiliriz. sadece document dan sonraki kismi kopyaliyoruz. console a yazip enter a tikliyoruz. sonra maus ile üzerine gidince sayfa da o element mavi oluyor.
+
+window.onload = function() {
+    // calisma esnasinda sürekli verileri girmemek icin böyle yapiyoruz.
+    email.value = "eve.holt@reqres.in";
+    password.value = "pistol";
+
+        // kullanicinin verilerini localstorage da kaydedecegiz:
+    
+
+}
+
+    submitBtn.onclick = () => {
+        postCustomerRegister();
+    }
+
+
+    const postCustomerRegister = async () => {
+        // ilk önce gönderecegimiz veriyi hazirliyoruz.
+        // buna bodydata diyecegiz postman de de ayni isim oldugu icin ve object tipinde
+
+        const bodyData = {
+            email : email.value,
+            password : password.value
+        };
+        // console.log(bodyData); /// axios oldugu icin
+        // fetch olsaydi su sekilde olacakti:
+        // console.log(JSON.stringify(bodyData));
+        // bu yapi yine axios da da kullanilabilir
+
+            // simdi post isleminde postman i taklit edecegiz. yani postman deki url, method ve data yi aynen yazacagiz.
+            // biz kullanici verilerini gönderdikten sonra api bize bir id bir de token gönderecek.
+            // bu nedenle asagidaki response degiskenimize biz bir axios tanimlamamoza ragmen bunun degeri artik id ve token olacak.
+            // bize gelen veri direkt gelmeyecek data icinde olacak o neenle destruckture gerekir.
+
+        try {
+            const response = await axios({
+                url : "https://reqres.in/api/register",
+                method : "post",
+                data : bodyData
+                
+            })
+            console.log(response);
+            const {data : userData} = response;
+            console.log(userData);
+
+                // simdi fetch icin hata olmayan bizim kendi error larimizi hazirliyoruz.
+                // bizim icin önemli olan id ve token.
+                // bunlari elde etmemizlazim. 
+                // url deki api yi silince o zaman hata catch e 404 olarak düsüyor. ama sadece son harfi silersek bu durumda bizim if imize düsüyor
+
+            if (userData.token == undefined) {
+                alert("undefined");
+                // bunu denemek icin yukarida url in sonundan bir harf siliyoruz.
+            } else {
+                localStorage.setItem("apiKey", EncryptStringAES(userData.token));
+
+                localStorage.setItem("baseUrl", EncryptStringAES("https://reqres.in/"));
+                // biz belki 100 tane api istegi gönderecegiz. her seferinde base url i yazmaya gerek yok. o nedenle database e yada local a kaydediyoruz oradan cekiyoruz.
+                // her ihtiyac oldugunda oradan cekmeliyiz
+                // amac cok fazla yerde kullanilacak verileri local de depoluyoruz
+                // kullanicinin ve baskalarinin bizim apimizi ve token i görmesine gerek yok o nedenle sifreliyoruz
+
+                // eger browser kapatildiginda silinsin istiyorsak local storage degil session storage kullanilir. kullanici diger sayfaya yönlendirildiginde silinsin istiyorsak da, sessionstorage.remove kullanabiliriz.
+                
+                removeLoading();
+
+                window.location.href = "userList.html";
+                // eger kullanici sign in oldu ise bu durumda kullaniciya izin veriyoruz kullanici listesini görmesi icin ve o sayfaya yönlendiriyoruz. 
+                // userlist in js dosyasina da gidip bu sifreyi cöz ve alert de bas diyoruz.
+
+                // sadece sayfa ismini yaziyoruz. url deki 127.0.0.1:5501/ kismini yazmiyoruz. bu bizim serverimizin yani sunucunun ip si veya port adresi
+
+            }
+            // apikey bir tokendir. bizim apiden veri talep edebilmemiz icin register yapmamiz lazim. aksi takdirde api bize veri vermez.
+
+
+
+
+
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    /// status code 200 olmazsa catch e düser
 
 
